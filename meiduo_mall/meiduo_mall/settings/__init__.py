d54d3ab0c1fd290e470b62a795f2f7adda0528f9
yi0506 <wang8023shiyi@gmail.com>
2020-11-13 17:20:12 +0800

    配置MySQL数据库

--- meiduo_mall/meiduo_mall/settings/__init__.py
+++ meiduo_mall/meiduo_mall/settings/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 if __name__ == '__main__':
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '192.168.72.129',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'yi0506',  # 数据库用户名
+        'PASSWORD': '211314',  # 数据库用户密码
+        'NAME': 'meiduo'  # 数据库名字
     }
 }
 
