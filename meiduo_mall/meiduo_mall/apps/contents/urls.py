a635c01a0bbf3ae2688fa24311fead7b0fa32938
yi0506 <wang8023shiyi@gmail.com>
2020-11-24 09:39:26 +0800

    短信验证码-后端接收校验参数

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++ meiduo_mall/meiduo_mall/apps/contents/urls.py
@@ -3,13 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # 浏览器会自动在路由的最后添加一个'/'，如果url路径参数中没有写的话
-
-    # 首页的路由为'/' 但是不需要写，因为根路径'/'会被django在总路由匹配后自动去除后，进行路由匹配
-    # 如果写成 r'^/$，那么路由就要写 http://127.0.0.1:8000// ，两个斜杠
-
-    # django在总路由匹配过之后，会将总路由从整个url路径参数中删除，只留下后面的部分，在子应用路由中匹配，从'/'之前开始删除，包括'/'
-    # 因为 根路径'/'在项目总路由中进行了匹配，django匹配成功后，会将'/'前面的路由自动去除掉
     # Django的路由是 头不写尾写
     url(r'^$', views.IndexView.as_view(), name='index'),
 
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -15,7 +15,27 @@ class SMSCodeView(View):
         :param mobile: 手机号码
         :return: JSON --> {code：errmsg}
         """
-        pass
+        # 接收参数
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        # 校验参数
+        # mobile不需要校验，因为路由中已经校验过了
+        # image_code_client不需要二次校验，因为后面会对比用户输入的图形验证码
+        # uuid也不需要二次校验，因此后面会通过uuid提取redis中的图形验证码
+        if not all([image_code_client, uuid]):
+            return http.HttpResponseForbidden('缺少必要参数')
+        # 接收图形验证码
+
+        # 删除图形验证码
+
+        # 对比图形验证码
+
+        # 生成短信验证码
+
+        # 保存图形验证码
+        # 发送短信验证码
+
+        # 响应结果
 
 
 class ImageCodeView(View):
