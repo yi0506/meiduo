01abc9ab520e027a890e22a5b440331361410428
yi0506 <244052886@qq.com>
2020-12-06 13:10:42 +0800

    用户中心-celery错误重试

--- meiduo_mall/celery_tasks/email/tasks.py
+++ meiduo_mall/celery_tasks/email/tasks.py
@@ -1,20 +1,29 @@
 # -*- coding: UTF-8 -*-
 from django.core.mail import send_mail
 from django.conf import settings
+from logging import getLogger
 
 from celery_tasks.main import celery_app
 
 
-@celery_app.task(name='send_verify_email')
-def send_verify_email(to_email, verify_url):
+logger = getLogger('django')
+
+
+@celery_app.task(bind=True, name='send_verify_email', retry_backoff=3)
+def send_verify_email(self, to_email, verify_url):
     """发送验证邮箱任务"""
     subject = '美多商城邮箱验证'
     html_message = '<p>尊敬的用户您好！</p>' \
                    '<p>感谢您使用美多商城。</p>' \
                    '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
                    '<p><a href="%s">%s<a></p>' % (to_email, verify_url, verify_url)
-    # send_mail('邮件标题', '普通邮件正文', '发件人', '收件人列表', '多媒体邮件正文')
-    send_mail(subject=subject, message='', from_email=settings.EMAIL_FROM, recipient_list=[to_email, ], html_message=html_message)
+    try:
+        # send_mail('邮件标题', '普通邮件正文', '发件人', '收件人列表', '多媒体邮件正文')
+        send_mail(subject=subject, message='', from_email=settings.EMAIL_FROM, recipient_list=[to_email, ], html_message=html_message)
+    except Exception as e:
+        # 出现错误，重试3次
+        logger.error(e)
+        raise self.retry(exc=e, max_retries=3)
 
 
 if __name__ == '__main__':
--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -11,7 +11,7 @@ logger = getLogger('django')
 
 
 # 使用装饰器装饰异步任务，保证celery能识别任务
-# bind：保证task对象会作为第一个参数自动传入
+# bind：保证task对象会作为第一个参数自动传入，task对象是当前任务的调用者
 # name：异步任务别名
 # retry_backoff：异常自动重试的时间间隔 第n次(retry_backoff×2^(n-1))s
 # max_retries：异常自动重试次数的上限
@@ -19,7 +19,7 @@ logger = getLogger('django')
 def send_sms_code(self, mobile, sms_code):
     """
     发送短信验证码的异步任务
-    :param self: 任务本身
+    :param self: 当前的task对象，只有写了bind=True，才能传self
     :param mobile: 手机号
     :param sms_code: 短信验证码
     :return 成功0，失败-1
@@ -32,10 +32,8 @@ def send_sms_code(self, mobile, sms_code):
         logger.error(e)
         # 有异常自动重试三次
         raise self.retry(exc=e, max_retries=3)
-    if result != 0:
-        # 有异常自动重试三次
-        raise self.retry(exc=Exception('发送短信失败'), max_retries=3)
-    return result
+    else:
+        return result
 
 
 if __name__ == '__main__':
