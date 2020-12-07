57dc3c56b9bbc44cf5360c6ed8240d1cf628481b
yi0506 <wang8023shiyi@gmail.com>
2020-12-07 16:56:20 +0800

    收货地址-省级模型列表转为字典列表

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -22,13 +22,18 @@ class AreaView(View):
             try:
                 # Area.objects.filter(属性名__条件表达式=值)
                 province_model_list = Area.objects.filter(parent__isnull=True)
+                # 将模型列表转换为字典列表
+                province_list = []
+                for province_model in province_model_list:
+                    province_dict = {
+                        'id': province_model.id,
+                        'name': province_model.name,
+                    }
+                    province_list.append(province_dict)
+                return http.JsonResponse({'code': RETCODE.OK, 'errmsg': err_msg[RETCODE.OK], 'province_list': province_list})
             except DatabaseError as e:
                 logger.error(e)
-                return http.JsonResponse({'code': '',
-                                          'errmsg': '',
-                                          'province_list': '',
-                                          })
-            pass
+                return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': err_msg[RETCODE.DBERR], 'province_list': []})
         else:
             # 市区级数据
             pass
