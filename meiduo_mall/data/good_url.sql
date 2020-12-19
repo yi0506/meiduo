82d10a479b9cb6512af3ca6d690197ea346d0d6f
yi0506 <244052886@qq.com>
2020-12-19 12:09:47 +0800

    修改首页url链接

---
+++ meiduo_mall/data/good_url.sql
@@ -0,0 +1,4 @@
+update meiduo.tb_goods_channel set url='https://nicemoe15.com//' where id!=1;
+update meiduo.tb_goods_channel set url='https://www.instagram.com/2km2km/' where id>15;
+update meiduo.tb_content set url='https://twitter.com/kokurachiyo' where 1;
+update meiduo.tb_content set url='https://twitter.com/misao_28' where id>66;
