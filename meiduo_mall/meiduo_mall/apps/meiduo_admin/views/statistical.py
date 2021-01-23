4ea2849286fcf8499d60199c56267ad155fa8621
yi0506 <244052886@qq.com>
2021-01-23 18:37:45 +0800

    用户统计类重构

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
@@ -7,58 +7,55 @@ from rest_framework.response import Response
 from users.models import User
 
 
-class UserCountView(APIView):
-    """用户数量统计"""
+class UserStatisticsView(APIView):
+    """用户数据统计类"""
     # 权限指定
     permission_classes = [IsAdminUser]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # 获取当天日期
+        self.meiduo_now_date = date.today()
 
     def get(self, request):
         """获取并返回用户总量与当天日期"""
-        # 获取当天日期
-        now_date = date.today()
         # 获取用户总量
-        count = User.objects.all().count()
+        count = self.get_user_count()
+        if count == -1:
+            raise NotImplementedError('获取所需用户数量未完成')
         # 返回结果
         return Response({
-            'date': now_date,
+            'date': self.meiduo_now_date,
             'count': count
         })
 
+    def get_user_count(self, *args, **kwargs):
+        """按照业务需要，获取用户数量"""
+        return -1
 
-class UserDayIncrementView(APIView):
+
+class UserCountView(UserStatisticsView):
+    """用户数量统计"""
+
+    def get_user_count(self):
+        """获取用户总量"""
+        return User.objects.all().count()
+
+
+class UserDayIncrementView(UserStatisticsView):
     """日增用户数量统计"""
-    # 权限指定
-    permission_classes = [IsAdminUser]
 
-    def get(self, request):
-        """获取并返回日增用户总量与当天日期"""
-        # 获取当天日期
-        now_date = date.today()
-        # 获取当天日增用户总量
-        count = User.objects.filter(date_joined__gte=now_date).count()
-        # 返回结果
-        return Response({
-            'date': now_date,
-            'count': count
-        })
+    def get_user_count(self):
+        """获取当天日增用户总量"""
+        return User.objects.filter(date_joined__gte=self.meiduo_now_date).count()
 
 
-class UserDayActiveView(APIView):
+class UserDayActiveView(UserStatisticsView):
     """日活用户数量统计"""
-    # 权限指定
-    permission_classes = [IsAdminUser]
 
-    def get(self, request):
-        """获取并返回日活用户总量与当天日期"""
-        # 获取当天日期
-        now_date = date.today()
-        # 获取当天登录用户总量
-        count = User.objects.filter(last_login__gte=now_date).count()
-        # 返回结果
-        return Response({
-            'date': now_date,
-            'count': count
-        })
+    def get_user_count(self):
+        """获取当天登录用户总量"""
+        return User.objects.filter(last_login__gte=self.meiduo_now_date).count()
 
 
 if __name__ == '__main__':
