7a94185492cd2ad2409f4aeef08e9ad0d70651f2
yi0506 <244052886@qq.com>
2021-01-24 15:15:02 +0800

    jwt_response_payload.py 移动位置

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -374,7 +374,7 @@ JWT_AUTH = {
     # JWT token 有效期
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
     # 指定JWT返回结果
-    'JWT_RESPONSE_PAYLOAD_HANDLER': 'meiduo_admin.utils.jwt_response_payload_handler',
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'meiduo_mall.utils.jwt_response_payload.jwt_response_payload_handler',
 }
 
 if __name__ == '__main__':
