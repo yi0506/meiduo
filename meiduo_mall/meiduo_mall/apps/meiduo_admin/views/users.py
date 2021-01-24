f588bbd2cd12ba6d8d6a3676df397101024f58f1
yi0506 <244052886@qq.com>
2021-01-24 18:37:00 +0800

    views.users.py 优化代码

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
@@ -8,15 +8,13 @@ from users.models import User
 
 class UserView(ListAPIView):
     """获取用户数据"""
-    # 指定查询集
-    queryset = User.objects.all()
     # 指定序列化器
     serializer_class = UserSerializer
     # 使用分页器
     pagination_class = UserPaginator
 
     def get_queryset(self):
-        """重写get_queryset方法，根据前端是否传递keyword值返回不同查询结果"""
+        """重写get_queryset方法，根据前端是否传递keyword值返回不同查询结果，得到查询集"""
         # 获取前端传递的keyword值
         keyword = self.request.query_params.get('keyword')
         # 如果keyword是空字符，则说明要获取所有用户数据
