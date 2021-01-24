e3b6a28402942c315f97ec8ff2296c2bb81a0d98
yi0506 <244052886@qq.com>
2021-01-24 22:09:20 +0800

    用户管理 -- 新增用户 验证手机号字段

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
@@ -1,11 +1,13 @@
 # -*- coding: UTF-8 -*-
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from users.models import User
 
 
 class UserSerializer(ModelSerializer):
     """用户模型类序列化器"""
+    mobile = serializers.RegexField(r'^1[3-9]\d{9}$')  # 验证手机号字段
 
     class Meta:
         model = User
@@ -21,6 +23,18 @@ class UserSerializer(ModelSerializer):
                 'min_length': 5,
             }
         }
+
+    def create(self, validated_data):
+        """继承并重写create方法，对password进行加密"""
+        # # 方法一：
+        # user = super(UserSerializer, self).create(validated_data)
+        # # 密码加密
+        # user.set_password(validated_data['password'])
+        # user.save()
+
+        # 方法二：
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 if __name__ == '__main__':
