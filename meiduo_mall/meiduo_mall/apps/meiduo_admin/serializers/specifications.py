695beeaeee00ef5293e297f213716f736cc42184
yi0506 <244052886@qq.com>
2021-01-25 14:03:01 +0800

    商品管理 -- 改写序列化器返回关联模型数据

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/specifications.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/specifications.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from goods.models import SPUSpecification
 
 
 class SpecsSerializer(ModelSerializer):
     """商品规格序列化器"""
+    # 指定关联模型的数据返回形式
+    spu = serializers.StringRelatedField(read_only=True)  # 根据关联模型的方式取数据
+    spu_id = serializers.IntegerField()  # 根据数据表中的外键字段取数据
 
     class Meta:
         model = SPUSpecification
