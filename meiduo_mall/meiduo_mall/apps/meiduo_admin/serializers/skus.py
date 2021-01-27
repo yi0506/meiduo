8e8bec4310b14d267f203bf2d43a673eb4a88a27
yi0506 <244052886@qq.com>
2021-01-27 16:59:24 +0800

    SKU管理 -- 使用事务保存sku与sku_specifications表信息

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
@@ -1,18 +1,60 @@
 # -*- coding: UTF-8 -*-
 from rest_framework import serializers
+from django.db import transaction
+from logging import getLogger
 
-from goods.models import SKU, GoodsCategory, SPUSpecification, SpecificationOption
+from goods.models import SKU, GoodsCategory, SPUSpecification, SpecificationOption, SKUSpecification
+
+
+logger = getLogger('django')
+
+
+class SKUSpecificationSerializer(serializers.ModelSerializer):
+    """SKU规格表序列化器"""
+
+    spec_id = serializers.IntegerField(read_only=True)
+    option_id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = SKUSpecification  # SKUSpecification中sku外键关联了SKU表
+        fields = ("spec_id", 'option_id')
 
 
 class SKUSerializer(serializers.ModelSerializer):
     """SKU序列化器"""
+    # 指定所关联的SKU规格选项信息 关联嵌套返回
+    specs = SKUSpecificationSerializer(read_only=True, many=True)
     spu_id = serializers.IntegerField()
     category_id = serializers.IntegerField()
 
     class Meta:
         model = SKU
         fields = '__all__'
-        read_only = ('spu', 'category')
+        read_only_fields = ('spu', 'category')
+
+    def create(self, validated_data):
+        """使用事务保存sku表信息与sku_specifications表信息"""
+        # sku的所有规格信息
+        sku_specs = self.context['request'].data.get('specs')
+        # 开启事务
+        with transaction.atomic():
+            # 设置保存点
+            save_point = transaction.savepoint()
+            try:
+                # 保存sku表
+                sku = SKU.objects.create(**validated_data)
+                # 保存sku_specifications表
+                for sku_spec in sku_specs:
+                    SKUSpecification.objects.create(spec_id=sku_spec['spec_id'], option_id=sku_spec['option_id'], sku=sku)
+            except Exception as e:
+                # 回滚
+                transaction.savepoint_rollback(save_point)
+                logger.error(e)
+                raise serializers.ValidationError('sku保存失败')
+            else:
+                # 提交事务
+                transaction.savepoint_commit(save_point)
+                return sku
 
 
 class GoodsCategorySerializer(serializers.ModelSerializer):
