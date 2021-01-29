18754543d99b8798a06fb24302df36ba546370ef
yi0506 <244052886@qq.com>
2021-01-29 17:25:12 +0800

    管理员管理 管理员增、删、改

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/admins.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/admins.py
@@ -16,3 +16,23 @@ class AdminSerializer(serializers.ModelSerializer):
             },
         }
 
+    def create(self, validated_data):
+        """重写保存数据方法"""
+        # 添加管理员字段
+        validated_data['is_staff'] = True
+        # 调用父类方法创建管理员用户
+        admin = super().create(validated_data)
+        # 用户密码加密
+        password = validated_data['password']
+        admin.set_password(password)
+        admin.save()
+        return admin
+
+    def update(self, instance, validated_data):
+        """重写更新方法"""
+        # 更新字段
+        user = super(AdminSerializer, self).update(instance, validated_data)
+        # 对密码进行加密
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
